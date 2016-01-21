8a840434b7f5d85a0e7577ad0f0aba77d188bda8
Eloy Vega <eloy.vega@gmail.com>
2016-01-21 12:07:52 -0600

    Add correct answer

--- 3_largest_prime_factor.py
+++ 3_largest_prime_factor.py
@@ -8,9 +8,34 @@ def is_prime(n):
             return False
     return True
 
-def largest_prime_factor(x):
+# My answer
+def largest_prime_factor_mine(x):
     for i in range(1,x+1):
         if x%i==0 and is_prime(x//i):
             return x//i
+
+# The answer
+def largest_prime_factor(n):
+    if n % 2 == 0:
+        n = n/2
+        last_factor = 2
+        while n % 2 == 0:
+            n = n/2
+    else:
+        last_factor = 1
+    factor = 3
+    max_factor = math.sqrt(n)
+    while n > 1 and factor <= max_factor:
+        if n%factor == 0:
+            last_factor = factor
+            n = n/factor
+            while n % factor == 0:
+                n = n/factor
+            max_factor = math.sqrt(n)
+        factor += 2
+    if n == 1:
+        return last_factor
+    else:
+        return int(n)
 
 print(largest_prime_factor(int(sys.argv[1])))
